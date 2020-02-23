c717a6021df2d4ebde9268dd0bbeffa36cbe64cc
memory-agape <memory.style.serv@gmail.com>
2020-02-23 11:48:45 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 brew install git
 
 # Clone this repository
-git clone --depth 1 git@github.com:memory-agape/dotfiles.git ~/dotfiles
+git clone --depth 1 git@github.com:m3m0r7/dotfiles.git ~/dotfiles
 
 # Move to current directory
 cd ~/dotfiles
@@ -28,16 +28,16 @@ if [ -f ~/.vimrc ]; then
 fi
 
 # Create symbolic links
-ln ~s .memory_chan ~/.memory_chan
-ln -s .zshrc ~/.zshrc
-ln -s .vimrc ~/.vimrc
-ln -s .gitignore_global ~/.gitignore_global
-ln -s .fdignore ~/.fdignore
-ln -s .original-scripts ~/.original-scripts
-ln -s webpack.config.js ~/webpack.config.js
+ln -nsf ~/dotfiles/.memory_chan ~/.memory_chan
+ln -nsf ~/dotfiles/.zshrc ~/.zshrc
+ln -nsf ~/dotfiles/.vimrc ~/.vimrc
+ln -nsf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -nsf ~/dotfiles/.fdignore ~/.fdignore
+ln -nsf ~/dotfiles/.original-scripts ~/.original-scripts
+ln -nsf ~/dotfiles/webpack.config.js ~/webpack.config.js
 
 # Install theme
-ln -s .oh-my-zsh/themes/memory.zsh-theme ~/.oh-my-zsh/themes/memory.zsh-theme
+ln -nsf ~/dotfiles/.oh-my-zsh/themes/memory.zsh-theme /Users/memory/.oh-my-zsh/themes/memory.zsh-theme
 
 # Source
 source ~/.zshrc
