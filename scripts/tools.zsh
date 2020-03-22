be31179d876d77aa92f155a18cd4a5e0ea6416bf
m3m0r7 <memory.style.serv@gmail.com>
2020-03-22 17:34:57 +0900

    Update

--- scripts/tools.zsh
+++ scripts/tools.zsh
@@ -2,7 +2,7 @@ make-brew-installed() {
   local path1 path2 oldpwd realpath
   realpath=$(readlink "$HOME/scripts")/..
   oldpwd=$(pwd)
-  path1=$(echo $realpath/scripts/.brew_cask_installed)
+  path1=$(echo $realpath/scripts/.brew_installed)
   path2=$(echo $realpath/scripts/.brew_cask_installed)
   brew list | awk "{print $7}" >"$path1"
   brew cask list | awk "{print $7}" >"$path2"
