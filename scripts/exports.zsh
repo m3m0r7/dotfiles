3e26921cbb8c5e5f062743e89dc0af6889e46dc2
m3m0r7 <memory.style.serv@gmail.com>
2021-08-31 22:25:35 +0900

    fix

--- scripts/exports.zsh
+++ scripts/exports.zsh
@@ -1,6 +1,6 @@
 export LANG=en_US.UTF-8
 export DISABLE_AUTO_UPDATE=true
-export PATH=/usr/local/opt/php@7.4/bin:/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin:$HOME/.nodebrew/current/bin:$HOME/.cargo/bin:$HOME/.original-scripts/bin:$HOME/.composer/vendor/bin:$HOME/bin:/usr/local/bin:/bin:$PATH
+export PATH=/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin:$HOME/.nodebrew/current/bin:$HOME/.cargo/bin:$HOME/.original-scripts/bin:$HOME/.composer/vendor/bin:$HOME/bin:/usr/local/bin:/bin:$PATH
 export ZSH="$HOME/.oh-my-zsh"
 export GOPATH=$HOME/.go
 export GO111MOD=on
