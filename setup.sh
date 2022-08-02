2dc0dea433a60890f5a5a02af47257877f466dd5
m3m0r7 <memory.style.serv@gmail.com>
2022-08-02 21:02:28 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 brew install git
 
 # Clone this repository
-git clone --depth 1 git@github.com:m3m0r7/dotfiles.git $HOME/dotfiles
+git clone --depth 1 git@github.com:m3m0r7/dotfiles.git /Volumes/develop/dotfiles
 
 # Move to current directory
-cd $HOME/dotfiles
+cd /Volumes/develop/dotfiles
 
 # Update brew
 brew tap homebrew/cask
@@ -34,8 +34,7 @@ ln -nsf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -nsf $HOME/dotfiles/scripts $HOME/scripts
 
 # Copy SSH config
-mkdir $HOME/.ssh && \
-  ln -nsf $HOME/dotfiles/ssh-config/ssh-config $HOME/.ssh/config
+cp $HOME/dotfiles/ssh-config/ssh-config $HOME/.ssh/config
 
 # Make sublime text settings
 mkdir $HOME/Library/Application Support/Sublime Text 3/Packages/Default && \
@@ -46,11 +45,14 @@ git clone https://github.com/wfxr/forgit.git $HOME/.oh-my-zsh/plugins/forgit
 git clone https://github.com/seebi/dircolors-solarized.git $HOME/.oh-my-zsh/plugins/dircolors-solarized
 $(brew --prefix)/opt/fzf/install
 
-git clone https://github.com/chitoku-k/fzf-zsh-completions $HOME/.oh-my-zsh/plugins/fzf-zsh-completions
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 git clone git@github.com:b4b4r07/enhancd.git $HOME/.oh-my-zsh/plugins/enhancd
 git clone git@github.com:chitoku-k/fzf-zsh-completions.git $HOME/.oh-my-zsh/plugins/fzf-zsh-completions
+
+git clone git@github.com:zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-completions $HOME/.oh-my-zsh/plugins/zsh-completions
+git clone git@github.com:zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
 # Source
 source $HOME/.zshrc
