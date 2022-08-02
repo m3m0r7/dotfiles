a2bf3de2f7030391e2e5f02b1e0d15f8799e7b6a
m3m0r7 <memory.style.serv@gmail.com>
2022-08-02 17:03:06 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env zsh
 
-# Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
